bd2574705bdc6633e64e449b1270824f644cbe0f
‘lordieXXL’ <caramorton40@gmail.com>
2025-08-28 16:20:20 +0100

    resend

--- deestincts/app/components/CompanyMarquee.tsx
+++ deestincts/app/components/CompanyMarquee.tsx
@@ -2,19 +2,7 @@
 
 import { motion } from 'framer-motion'
 import Image from 'next/image'
-
-const companies = [
-  { name: 'spitch', logo: '/marquee/spitch.svg' },
-  { name: 'florence', logo: '/marquee/florence.png' },
-  { name: 'kelvin', logo: '/marquee/kelvin.svg'},
-  { name: 'oosha', logo: '/marquee/oosha.svg'},
-  { name: 'outside', logo: '/marquee/outside.svg' },
-  { name: 'beeyond', logo: '/marquee/beeyond.svg' },
-  { name: 'chop', logo: '/marquee/chop.svg' },
-  { name: 'delani', logo: '/marquee/delani.png' },
-  { name: 'figo', logo: '/marquee/figo.svg' },
-  { name: 'formular', logo: '/marquee/formular.svg' },
-]
+import Marquee from "react-fast-marquee";
 
 export default function CompanyMarquee() {
   return (
@@ -28,36 +16,20 @@ export default function CompanyMarquee() {
         >
           Trusted by industry leaders
         </motion.p>
-        
+
         <div className="overflow-hidden">
-          <motion.div
-            animate={{ x: [0, -100 * companies.length] }}
-            transition={{
-              x: {
-                repeat: Infinity,
-                repeatType: "loop",
-                duration: 20,
-                ease: "linear",
-              },
-            }}
-            className="flex space-x-16"
-            style={{ width: `${200 * companies.length}%` }}
-          >
-            {[...companies, ...companies].map((company, index) => (
-              <div
-                key={`${company.name}-${index}`}
-                className="flex-shrink-0 flex items-center justify-center h-16 w-32"
-              >
-                <Image
-                  src={company.logo}
-                  alt={company.name}
-                  width={120}
-                  height={60}
-                  className="max-h-12 w-auto object-contain filter grayscale opacity-60 hover:opacity-100 transition-opacity duration-300"
-                />
-              </div>
-            ))}
-          </motion.div>
+          <Marquee>
+            <Image src="/marquee/spitch.svg" alt="spitch" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300 mx-6 md:mx-8" />
+            <Image src="/marquee/florence.png" alt="florence" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300 mx-6 md:mx-8" />
+            <Image src="/marquee/kelvin.svg" alt="kelvin" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300 mx-6 md:mx-8" />
+            <Image src="/marquee/oosha.svg" alt="oosha" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300 mx-6 md:mx-8" />
+            <Image src="/marquee/outside.svg" alt="outside" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300 mx-6 md:mx-8" />
+            <Image src="/marquee/beeyond.svg" alt="beeyond" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300 mx-6 md:mx-8" />
+            <Image src="/marquee/chop.svg" alt="chop" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300 mx-6 md:mx-8" />
+            <Image src="/marquee/delani.png" alt="delani" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300 mx-6 md:mx-8" />
+            <Image src="/marquee/figo.svg" alt="figo" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300 mx-6 md:mx-8" />
+            <Image src="/marquee/formular.svg" alt="formular" width={160} height={48} className="h-10 md:h-12 w-auto object-contain filter grayscale opacity-70 hover:opacity-100 transition-opacity duration-300" />
+          </Marquee>
         </div>
       </div>
     </section>
